15fa8b83ac65af7d64f94d4609cbaf02937b53fb
Abhishek90766398 <113997298+Abhishek90766398@users.noreply.github.com>
2023-10-20 15:59:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+displayTime();
+function displayTime(){
+    let dateTime = new Date();
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    let session = document.getElementById('session');
+
+    // if(hrs>=12){
+    //     session.innerHTML = 'PM';
+    // }else{
+    //     session.innerHTML = 'AM';
+    // }
+    // if(hrs>12){
+    //     hrs = hrs - 12;
+    // }
+
+console.log(hrs);
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+    //document.getElementsById('hours').innerHTML=hrs;
+    document.getElementById('hours').innerHTML=hrs;
+}
+setInterval(displayTime, 1000);
